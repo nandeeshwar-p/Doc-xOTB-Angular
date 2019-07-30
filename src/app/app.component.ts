20dae17cfa47b9b9cc58f122451332b0d6efcf18
vijay kumar navulla <vijaykumarnavulla@gmail.com>
2019-07-30 20:56:53 +0530

    completed the accordion documentation.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -38,9 +38,45 @@ export class AppComponent {
   selectorOptions1:ISelectorOptions;
   modalOptions:IModalOptions;
 
+
+  public title1;
+  public title2;
+  public title3;
+  public title4;
+  public openFirst1;
+  public toggle1;
+  public openFirst2;
+  public toggle2;
+  public description1;
+  public description2;
+
   constructor(private _http: HttpClient){}
 
   ngOnInit() {
+
+    this.title1 = 'Dashboard';
+    this.title2 = 'Calendar';
+    this.title3 = 'Folder';
+    this.title4 = 'Shopping Cart';
+    this.description1 = 'Click on + to expand, - to collapse';
+    this.description2 = 'Click on + to expand current, collapse other';
+    // this.icon1 = 'fas fa-book';
+    // this.icon2 = 'fas fa-calendar';
+    // this.icon3 = 'fas fa-folder';
+    // this.icon4 = 'fas fa-shopping-cart';
+    // this.open1 = 'fas fa-plus';
+    // this.open2 = 'fas fa-calendar-plus';
+    // this.open4 = 'fas fa-plus-circle';
+    // this.open3 = 'fas fa-folder-plus';
+    // this.close1 = 'fas fa-minus';
+    // this.close2 = 'fas fa-calendar-minus';
+    // this.close4 = 'fas fa-minus-circle';
+    // this.close3 = 'fas fa-folder-minus';
+    this.openFirst1 = false;
+    this.toggle1 = false;
+    this.openFirst2 = true;
+    this.toggle2 = true;
+
     this.getFormInfo();
     this.getImages();
     this.getFormInfoCustom();
@@ -123,6 +159,10 @@ export class AppComponent {
     this.onSelectTab('multiSelvalue');
   }
 
+  onChangeAccordionToggle(obj){
+    console.log(obj);
+  }
+  
   onActionButtonClick(buttonName) {
     switch (buttonName) {
       case 'demo':
--- src/app/modules/accordion-group/accordion-doc.component.ts
+++ src/app/modules/accordion-group/accordion-doc.component.ts
@@ -15,10 +15,16 @@ export class AccordionDocComponent implements OnInit {
   public outputDisplayedColumns:any = ['MethodName', 'EventType', 'Description'];
 
   private input_element_data: IinputElement[] = [
-    {attribute: "title", datatype: 'String', default: "accordion", description: 'It will set the header title'},
+    {attribute: "openFirst", datatype: 'boolean', default: "false", description: 'It will set the first accordion open true or false'},
+    {attribute: "title", datatype: 'string', default: "", description: 'It will set the header title'},
+    {attribute: "description", datatype: 'string', default: "", description: 'It will set the header title'},
+    {attribute: "icon", datatype: 'string', default: "", description: 'It will set icon class'},
+    {attribute: "open", datatype: 'string', default: "", description: 'It will set open icon class'},
+    {attribute: "close", datatype: 'string', default: "", description: 'It will set close icon class'},
   ];
 
   private output_element_data: IoutputElement[] = [
+    {methodName: "toggle", eventType: 'object', description: 'Emitted when user clicks on group titlebar'},
   ];
 
   public input_dataSource = new MatTableDataSource<IinputElement>(this.input_element_data);
@@ -41,20 +47,69 @@ export class AccordionDocComponent implements OnInit {
     this.example = `
     Sample example
 
-    <div fxLayout="column" fxLayoutGap="20px">
-      <accordion-group [title]="'first accordion'">
-        <h4>content</h4>
+    <app-accordion [openFirst]="openFirst1" (toggle)="onChangeAccordionToggle($event)>
+      <accordion-group [title]="title1" [description]="description1" [icon]="icon1" [open]="open1" [close]="close1">
+          <div class="form-group">
+              <label for="email">Email:</label>
+              <input type="email" class="form-control" id="email" placeholder="Enter email" name="email">
+              </div>
+              <div class="form-group">
+              <label for="pwd">Password:</label>
+              <input type="password" class="form-control" id="pwd" placeholder="Enter password" name="pwd">
+              </div>
+              <div class="checkbox">
+              <label><input type="checkbox" name="remember"> Remember me</label>
+              </div>
+              <button type="submit" class="btn btn-default">Submit</button>            
       </accordion-group>
-      <accordion-group [title]="'second accordion'">
-        <h4>content</h4>
+      <accordion-group [title]="title2" [description]="description1" [icon]="icon2" [open]="open2" [close]="close2">
+              <div class="btn-group" role="group" aria-label="Button group with nested dropdown">
+                      <button type="button" class="btn btn-secondary">1</button>
+                      <button type="button" class="btn btn-secondary">2</button>
+                      
+                      <div class="btn-group" role="group">
+                          <button id="btnGroupDrop1" type="button" class="btn btn-secondary dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                          Dropdown
+                          </button>
+                          <div class="dropdown-menu" aria-labelledby="btnGroupDrop1">
+                          <a class="dropdown-item" href="#">Dropdown link</a>
+                          <a class="dropdown-item" href="#">Dropdown link</a>
+                          </div>
+                      </div>
+                      </div>                 
       </accordion-group>
-      <accordion-group [title]="'third accordion'">
-        <h4>content</h4>
-      </accordion-group>
-      <accordion-group [title]="'four accordion'">
-        <h4>content</h4>
-      </accordion-group>
-    </div>
+  </app-accordion>
+
+  Sample custom example
+
+  <app-accordion [openFirst]="openFirst2" (toggle)="onChangeAccordionToggle($event)>
+    <accordion-group [title]="title3" [description]="description2" [icon]="icon3" [open]="open3" [close]="close3">
+            <form class="form-horizontal">
+                    <div class="form-group">
+                      <label class="control-label col-sm-2" for="email">Email:</label>
+                      <div class="col-sm-10">
+                        <p class="form-control-static">someone@example.com</p>
+                      </div>
+                    </div>
+                  </form>  
+    </accordion-group>
+    <accordion-group [title]="title4" [description]="description2" [icon]="icon4" [open]="open4" [close]="close4">
+      <form>
+        <div class="form-group">
+        <label for="inputsm">Small input</label>
+        <input class="form-control input-sm" id="inputsm" type="text">
+        </div>
+        <div class="form-group">
+        <label for="inputdefault">Default input</label>
+        <input class="form-control" id="inputdefault" type="text">
+        </div>
+        <div class="form-group">
+        <label for="inputlg">Large input</label>
+        <input class="form-control input-lg" id="inputlg" type="text">
+        </div>
+      </form>      
+    </accordion-group>
+  </app-accordion>     
     `;
   }
 }
